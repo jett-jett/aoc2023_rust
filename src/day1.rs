592005b3a6f587a9e413eba30a97385795bdf984
jett-jett <jett.ptacek@gmail.com>
2024-04-11 16:55:15 -0500

    Setup for question2 of day1

--- src/day1.rs
+++ src/day1.rs
@@ -28,11 +28,54 @@ pub fn question1() -> i32 {
             if left_found && right_found {
                 break;
             }
-            print!("{} {} :", left, right);
         }
         count += (left_num  * 10 )+ right_num;
-        println!("{}", (left_num * 10 ) + right_num);
     }
     count
 }
 
+pub fn question2() -> i32 {
+    let mut sum = 0;
+    for line in fs::read_to_string("input/day1.txt").unwrap().lines() {
+        let new_line = line_to_digits(line); 
+       
+        let mut il = 0;
+        let mut ir = 0;
+        let mut numl :i32 = 0;
+        let mut numr :i32 = 0;
+        let mut foundl = false;
+        let mut foundr = false;
+        
+        while ir >= il {
+
+            if new_line.as_bytes()[il].is_ascii_digit() {
+                if !foundl {
+                    numl = (new_line.as_bytes()[il] - b'0').into();
+                    foundl = true;
+                }
+            }else{
+                il += 1;
+            }
+ 
+            if new_line.as_bytes()[ir].is_ascii_digit() {
+                if !foundr {
+                    numr = (new_line.as_bytes()[ir] - b'0').into();
+                    foundr = true;
+                }
+            }else{
+                ir -= 1;
+            }
+            if foundr && foundl {
+                break;
+            }
+        }
+        sum += (numl * 10) + numr;
+    }
+    sum
+}
+
+
+fn line_to_digits(line: &str) -> &str {
+
+    line
+}
