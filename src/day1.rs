6a8da924faeb7622adeb6e619202c6d5f781a6bd
jett-jett <jett.ptacek@gmail.com>
2024-04-12 13:19:05 -0500

    Reworked day1 and finished both questions

--- src/day1.rs
+++ src/day1.rs
@@ -1,81 +1,39 @@
-use std::fs;
-
 pub fn question1() -> i32 {
-    let mut count = 0;
-    for line in fs::read_to_string("input/day1.txt").unwrap().lines() {
-        let mut left = 0;
-        let mut right = line.len() - 1; 
+    include_bytes!("../input/day1.txt")
+        .split(|b| b == &b'\n')
+        .map(|line| {
+            ((line.iter().find(|b| b.is_ascii_digit()).unwrap() - b'0') * 10
+             + line.iter().rev().find(|b| b.is_ascii_digit()).unwrap() -b'0') as usize 
+        })
+        .sum::<usize>() as i32
         
-        let mut right_num :i32 = 0;
-        let mut left_num :i32 = 0;
-        let mut left_found = false;
-        let mut right_found = false;
-        while right >= left {
-            if line.as_bytes()[right].is_ascii_digit() {
-                right_num = (line.as_bytes()[right] - b'0').into();
-                right_found = true;
-            }
-            else {
-                right -= 1;
-            }
-            if line.as_bytes()[left].is_ascii_digit() {
-                left_num = (line.as_bytes()[left] - b'0').into();
-                left_found = true;
-            }else{
-                left += 1;
-            }
-
-            if left_found && right_found {
-                break;
-            }
-        }
-        count += (left_num  * 10 )+ right_num;
-    }
-    count
 }
+
+const NUMS: [&[u8]; 9] = [
+    b"one", b"two", b"three", b"four", b"five", b"six", b"seven", b"eight", b"nine",
+];
 
 pub fn question2() -> i32 {
-    let mut sum = 0;
-    for line in fs::read_to_string("input/day1.txt").unwrap().lines() {
-        let new_line = line_to_digits(line); 
-       
-        let mut il = 0;
-        let mut ir = 0;
-        let mut numl :i32 = 0;
-        let mut numr :i32 = 0;
-        let mut foundl = false;
-        let mut foundr = false;
-        
-        while ir >= il {
+    include_bytes!("../input/day1.txt")
+            .split(|b| b == &b'\n')
+            .map(|line| {
+                (0..line.len()).find_map(|i| num(line, i)).unwrap() * 10
+                    + (0..line.len()).rev().find_map(|i| num(line, i)).unwrap()
+            })
+            .sum::<usize>() as i32
+}
 
-            if new_line.as_bytes()[il].is_ascii_digit() {
-                if !foundl {
-                    numl = (new_line.as_bytes()[il] - b'0').into();
-                    foundl = true;
-                }
-            }else{
-                il += 1;
-            }
- 
-            if new_line.as_bytes()[ir].is_ascii_digit() {
-                if !foundr {
-                    numr = (new_line.as_bytes()[ir] - b'0').into();
-                    foundr = true;
-                }
-            }else{
-                ir -= 1;
-            }
-            if foundr && foundl {
-                break;
-            }
-        }
-        sum += (numl * 10) + numr;
-    }
-    sum
+#[inline(always)]
+fn num(line: &[u8], i: usize) -> Option<usize> {
+    line[i]
+        .is_ascii_digit()
+        .then_some( if line[i] >= b'0' {(line[i] - b'0') as usize} else { line[i] as usize })
+        .or(NUMS
+            .iter()
+            .enumerate()
+            .find(|(_, name)| line[i..].starts_with(name))
+            .map(|(num, _)| num + 1))
 }
 
 
-fn line_to_digits(line: &str) -> &str {
 
-    line
-}
--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,5 @@ mod day1;
 
 fn main() {
     println!("Day1 1st Answer {}", day1::question1());
-    println!("Day1 1st Answer {}", day1::question2());
+    println!("Day1 2nd Answer {}", day1::question2());
 }
