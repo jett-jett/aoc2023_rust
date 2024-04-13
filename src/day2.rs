2b5693893a8e44e09a4c838c387776c368d3afb5
jett-jett <jett.ptacek@gmail.com>
2024-04-13 13:40:30 -0500

    Finished day2 q1

--- src/day2.rs
+++ src/day2.rs
@@ -1,3 +1,25 @@
+
 pub fn q1() -> i32 {
 
+    include_bytes!("../input/day2.txt").split(|b| b == &b'\n')
+        .enumerate()
+        .filter_map(|(game_id, line)| {
+            let mut rgb = [0,0,0,0];
+            line.splitn(2, |b| b == &b':')
+                .nth(1)
+                .unwrap()
+                .split(|b| b == &b',' || b == &b';')
+                .all(|item| {
+                    let i = match item[1..].splitn(2, |b| *b == b' ').nth(1).unwrap() {
+                        b"red" => 0usize,
+                        b"green" => 1,
+                        b"blue" => 2,
+                        _ => 3
+                    };
+                    if i < 4 {rgb[i] = rgb[i].max(atoi::atoi(&item[1..]).unwrap());} else{ rgb[i] = 0;}
+                    rgb[i] <= 12 + i as u32
+            })
+            .then_some(game_id +1)
+        })
+        .sum::<usize>() as i32
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod day1;
-
+mod day2;
 fn main() {
-    println!("Day1 1st Answer {}", day1::question1());
-    println!("Day1 2nd Answer {}", day1::question2());
+    //println!("Day1 1st Answer {}", day1::question1());
+    //println!("Day1 2nd Answer {}", day1::question2());
+    println!("Day2 1st Answer {}", day2::q1());
 }
