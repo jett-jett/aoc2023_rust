5e21bbf0608c9035f677823715bcd1585c9fb96f
jett-jett <jett.ptacek@gmail.com>
2024-04-16 13:47:08 -0500

    Solved day2 q2

--- src/day2.rs
+++ src/day2.rs
@@ -23,3 +23,28 @@ pub fn q1() -> i32 {
         })
         .sum::<usize>() as i32
 }
+
+pub fn q2() -> i32 {
+    include_bytes!("../input/day2.txt").split(|b| b == &b'\n')
+        .enumerate()
+        .filter_map(|(_, mut line)| {
+            let mut rgbmax = [0,0,0];
+            line = line.strip_suffix(&[b'\r']).unwrap_or(line);
+            line.splitn(2, |b| b == &b':')
+                .nth(1)
+                .unwrap()
+                .split(|b| b == &b',' || b == &b';')
+                .all(|item| {
+                    let i = match item[1..].splitn(2, |b| *b == b' ').nth(1).unwrap() {
+                        b"red" => 0usize,
+                        b"green" => 1,
+                        b"blue" => 2,
+                        _ => unreachable!()
+                    };
+                    rgbmax[i] = rgbmax[i].max(atoi::atoi(&item[1..]).unwrap());
+                    true
+            })
+            .then_some(rgbmax[0] * rgbmax[1] * rgbmax[2])
+        })
+    .sum::<usize>() as i32
+}
--- src/main.rs
+++ src/main.rs
@@ -4,4 +4,6 @@ fn main() {
     //println!("Day1 1st Answer {}", day1::question1());
     //println!("Day1 2nd Answer {}", day1::question2());
     println!("Day2 1st Answer {}", day2::q1());
+    println!("Day2 1st Answer {}", day2::q2());
+
 }
